f0507aab368cdd762a79d409d243f09419e849c2
rhiitola <47589000+rhiitola@users.noreply.github.com>
2023-08-20 17:22:46 +0300

    Fixed raster loading and added bbox creator func

--- load_and_combine_raster.py
+++ load_and_combine_raster.py
@@ -2,18 +2,46 @@
 # This helper code downloads multiple height data rasters, and combines them into one
 
 import rasterio
+from rasterio.merge import merge
 import requests
 from api_key import API_KEY
 
+def create_bbox(center=None, outer_bounds=None, max_raster_dim=20000):
+    if center is not None:
+        x_center, y_center = center
+        half_width = int(max_raster_dim / 2)
+        bbox = {
+            "nw": [x_center - half_width, y_center,
+                   x_center, y_center + half_width],
+            "sw": [x_center - half_width, y_center - half_width,
+                   x_center, y_center],
+            "ne": [x_center, y_center,
+                   x_center + half_width, y_center + half_width],
+            "se": [x_center, y_center - half_width,
+                   x_center + half_width, y_center]
+        }
+        return bbox
+    elif outer_bounds is not None:
+        x_min, y_min, x_max, y_max = outer_bounds
+        if x_max - x_min > max_raster_dim or y_max - y_min > max_raster_dim:
+            raise ValueError("Outer bounds are too far apart for merging")
+        bbox = {
+            "nw": [x_min, y_max - max_raster_dim, x_max - max_raster_dim, y_max],
+            "sw": [x_min, y_min + max_raster_dim, x_max - max_raster_dim, y_min],
+            "ne": [x_min + max_raster_dim, y_max - max_raster_dim, x_max, y_max],
+            "se": [x_min + max_raster_dim, y_min + max_raster_dim, x_max, y_min]
+        }
+        return bbox
+    else:
+        raise ValueError("Either center or outer_bounds should be provided")
 
-
-bbox = {
-    "nw" : [321194,6820945,330149,6827099],
-    "sw" : [321194,6814792,330149,6820945],
-    "ne" : [330149,6820945,339103,6827099],
-    "se" : [330149,6814792,339103,6820945]
-}
-paths = ["nw.tiff", "ne.tiff", "sw.tiff", "se.tiff"]
+# bbox = {
+#     "nw" : [321194,6820945,330149,6827099],
+#     "sw" : [321194,6814792,330149,6820945],
+#     "ne" : [330149,6820945,339103,6827099],
+#     "se" : [330149,6814792,339103,6820945]
+# }
+paths = ["nw.tif", "ne.tif", "sw.tif", "se.tif"]
 
 def read_height_data(bbox, api_key):
     for box, coords in bbox.items():
@@ -23,7 +51,7 @@ def read_height_data(bbox, api_key):
         # print(url)
         r = requests.get(url, stream = True)
 
-        with open(f"{box}.tiff","wb") as geotiff:
+        with open(f"{box}.tif","wb") as geotiff:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     geotiff.write(chunk)
@@ -37,7 +65,7 @@ def combine_rasters(paths, mosaic_name):
         src_files_to_mosaic.append(src)
 
     # Merge the four geotiffs into a single raster
-    mosaic, out_trans = rasterio.merge.merge(src_files_to_mosaic)
+    mosaic, out_trans = merge(src_files_to_mosaic)
 
     # Update the metadata for the mosaic
     out_meta = src.meta.copy()
@@ -51,5 +79,6 @@ def combine_rasters(paths, mosaic_name):
     with rasterio.open(mosaic_name, "w", **out_meta) as dest:
         dest.write(mosaic)
 
+bbox = create_bbox(center=[330149,6827099])
 read_height_data(bbox, API_KEY)
 combine_rasters(paths, 'mosaic.tif')
